ad0473477ac5661ab41f2ee3fd8368b3c3e9f9ce
Winston Weinert <winston@ml1.net>
2019-11-02 14:46:29 -0500

    make database work

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,42 @@
 from flask_api import FlaskAPI
+import sqlite3
+from flask import g
 
 app = FlaskAPI(__name__)
 
-@app.route('/example/')
-def example():
-    return {'hello': 'world'}
+
+DATABASE = './database.sqlite3'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+            db.commit()
+
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+@app.route('/api/technician')
+def technician():
+    return query_db('SELECT techID, locationZip, techTypeID, rating, contactNumber FROM Tech')
+
+
+@app.route('/api/skill')
+def skills():
+    return {}
--- backend/db.py
+++ backend/db.py
@@ -2,16 +2,19 @@ import sqlite3
 from flask import g
 from .app import app
 
+
 DATABASE = './database.sqlite3'
+
 
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
+    with open('create_tables.sql') as f:
+        c = db.cursor()
+        c.executescript(f.read())
     return db
 
-def get_tech(techName):
-    pass
 
 @app.teardown_appcontext
 def query_db(query, args=(), one=False):
@@ -19,6 +22,7 @@ def query_db(query, args=(), one=False):
     rv = cur.fetchall()
     cur.close()
     return (rv[0] if rv else None) if one else rv
+
 
 def close_connection(exception):
     db = getattr(g, '_database', None)
